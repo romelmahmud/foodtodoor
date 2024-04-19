d379fa6fdaacc721ed13a53393601a373474e324
romel <romelmahmud.dev@gmail.com>
2024-04-19 17:54:15 +0600

    layout setup

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<span>Home Page</span>} />
+      <Route path="/" element={<Layout>Home Page</Layout>} />
       <Route path="/about" element={<span>About Page</span>} />
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
